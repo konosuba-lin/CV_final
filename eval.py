30859ada2c72a092e8bae97bb910db462029db0b
vncntlin <vncntlintw@gmail.com>
2022-06-03 18:38:17 +0800

    restore eval to previous version

--- eval.py
+++ eval.py
@@ -10,6 +10,7 @@ from tqdm import tqdm
 
 from tool import load_parameters, NME_loss, fixed_seed, random_rotate
 from MobileNet_v3 import MobileNetV3
+from ShuffleNet import shufflenetv2
 from dataset import get_dataset
 from cfg import cfg
 import numpy as np
@@ -22,34 +23,59 @@ import random
 
 # The function help you to calculate accuracy easily
 # Normally you won't get annotation of test label. But for easily testing, we provide you this.
-def draw(test_set,idx,model,device,save_path):
+def draw(test_set,idx,model,device):
     path = os.path.join(test_set.prefix, test_set.images[idx])
-    img = Image.open(path)
+    image = Image.open(path)
+    data = test_set.transform(image)
     label = np.array(test_set.labels[idx])
-    img, label = random_rotate(img, label)
-    origin_img = cv2.cvtColor(np.asarray(img),cv2.COLOR_RGB2BGR)
+    T = transforms.ToPILImage()
+    img = T(data)
+    img = cv2.cvtColor(np.asarray(image),cv2.COLOR_RGB2BGR)
     for x, y in label:
-        cv2.circle(origin_img, (int(x), int(y)), 2, (0, 0, 255), -1)
+        cv2.circle(img, (int(x), int(y)), 2, (0, 0, 255), -1)
     label = label.flatten()
-    label = torch.tensor(label).view(68,2)
-    label = label.to(device)
-
-    itr = 1
-    for i in range(itr):
-        data = test_set.transform(img)
+    label = torch.tensor(label)
+    model.eval()
+    with torch.no_grad():
         model.eval()
-        with torch.no_grad():
-            model.eval()
-            data = data.to(device)
-            output = model(data[None, ...]).view(68,2)
-            for x, y in output:
-                cv2.circle(origin_img, (int(x), int(y)), 2, (i*255//itr, 0, 0), -1)
-            if(i==0): output_sum = output
-            else: output_sum += output
-        img = TF.adjust_gamma(img,gamma=0.3)
-    output = output_sum/itr
-    cv2.imwrite(save_path, origin_img)
-    print(NME_loss(output,label))
+        data = data.to(device)
+        label = label.to(device)
+        output = model(data[None, ...])
+        output = output.view(68,2)
+    for x, y in output:
+        cv2.circle(img, (int(x), int(y)), 2, (0, 255, 0), -1)
+    cv2.imwrite(test_set.images[idx].replace('image', 'test'), img)
+    print(test_set.images[idx], "NME: ", NME_loss(output,label)*100)
+
+# def draw(test_set,idx,model,device,save_path):
+#     path = os.path.join(test_set.prefix, test_set.images[idx])
+#     img = Image.open(path)
+#     label = np.array(test_set.labels[idx])
+#     img, label = random_rotate(img, label)
+#     origin_img = cv2.cvtColor(np.asarray(img),cv2.COLOR_RGB2BGR)
+#     # for x, y in label:
+#     #     cv2.circle(origin_img, (int(x), int(y)), 2, (0, 0, 255), -1)
+#     label = label.flatten()
+#     label = torch.tensor(label).view(68,2)
+#     label = label.to(device)
+
+#     itr = 1
+#     for i in range(itr):
+#         data = test_set.transform(img)
+#         model.eval()
+#         with torch.no_grad():
+#             model.eval()
+#             data = data.to(device)
+#             output = model(data[None, ...]).view(68,2)
+#             # for x, y in output:
+#             #     cv2.circle(origin_img, (int(x), int(y)), 2, (i*255//itr, 0, 0), -1)
+#             if(i==0): output_sum = output
+#             else: output_sum += output
+#         img = TF.adjust_gamma(img,gamma=0.3)
+#     output = output_sum/itr
+#     # cv2.imwrite(save_path, origin_img)
+#     # print(NME_loss(output,label))
+#     return NME_loss(output,label)
 
 def validation_test(data_loader, model, device):
     nme = 0;
@@ -101,7 +127,8 @@ def main():
 
     ## TO DO ## 
     # Indicate the model you use here
-    model = MobileNetV3(model_mode="LARGE", num_classes=136, multiplier=0.75)
+    # model = MobileNetV3(model_mode="LARGE", num_classes=136, multiplier=0.75)
+    model = shufflenetv2(num_classes=136)
     
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     #device = torch.device('cpu')
@@ -113,13 +140,12 @@ def main():
     val_set = get_dataset(root=eval_root)    
     val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False)
 
-    acc = validation_test(data_loader=val_loader, model=model, device=device)
-    print("accuracy : ", acc)
+    # acc = validation_test(data_loader=val_loader, model=model, device=device)
+    # print("accuracy : ", acc)
 
-    #print(img)
-    idx = 163
-    test_set = get_dataset(root=eval_root,aug=False) 
-    draw(test_set,idx,model,device,save_path="test.png")
+    # idx = 163
+    # test_set = get_dataset(root=eval_root,aug=False) 
+    # draw(test_set,idx,model,device,save_path="test.png")
 
     '''MSE = []
     for i in range(199):
@@ -127,15 +153,15 @@ def main():
     MSE = np.array(MSE)
     print(max(MSE),np.argmax(MSE))'''
     
-    # acc = validation_test(data_loader=val_loader, model=model, device=device)
-    # print("validation NME: ", (1 - acc)*100)
+    acc = validation_test(data_loader=val_loader, model=model, device=device)
+    print("validation NME: ", (1 - acc)*100)
     
-    # ##### choose your image number to be test #####
-    # # you can choose the test image you want
-    # test_image = random.randint(0, len(val_set)-1)
-    # # test_image = 2
-    # print("\n######## testing random sample... ########")
-    # draw(val_set, test_image, model, device)
+    ##### choose your image number to be test #####
+    # you can choose the test image you want
+    test_image = random.randint(0, len(val_set)-1)
+    # test_image = 2
+    print("\n######## testing random sample... ########")
+    draw(val_set, test_image, model, device)
 
     ##### generate solution #####
     print("\n######## generating solution... ########")
